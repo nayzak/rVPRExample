0d627732a6804d7b43392ac3978188d3168c8ce9
Ian Kazlauskas <nayzak90@gmail.com>
2016-08-18 18:22:27 +0900

    save

--- bin/gen.sh
+++ bin/gen.sh
@@ -2,6 +2,10 @@
 
 command=$1
 module_name=$2
+custom_path=$3
+
+project_name="rVPRExample"
+project_tests_target_name="rVPRExampleTests"
 
 if [[ -z $command || -z $module_name ]]; then
   echo "command or module name not specified"
@@ -10,7 +14,7 @@ fi
 
 if [[ $command == "usecase" ]]; then
   generamba gen $module_name rusecase --module-targets 'UseCases' --module-path UseCases --test-targets 'UseCasesTests' --test-path UseCasesTests
-  generamba gen $module_name rusecase-assembly --module-targets 'rVPRExample' --module-path Assemblies/UseCases  --test-targets 'rVPRExampleTests' --test-path rVPRExampleTests/Assemblies/UseCases
+  generamba gen $module_name rusecase-assembly --module-targets $project_name --module-path Assemblies/UseCases  --test-targets $project_tests_target_name --test-path $project_tests_target_name/Assemblies/UseCases
   exit 0
 fi
 
@@ -18,7 +22,21 @@ if [[ $command == "rm-usecase" ]]; then
   rm -rf Assemblies/UseCases/$module_name
   rm -rf UseCases/$module_name
   rm -rf UseCasesTests/$module_name
-  rm -rf rVPRExampleTests/Assemblies/UseCases/$module_name
+  rm -rf $project_tests_target_name/Assemblies/UseCases/$module_name
+  exit 0
+fi
+
+if [[ $command == "vpr-xcontroller" ]]; then
+  generamba gen $module_name rvpr-xcontroller --module-targets 'UI' --module-path UITests/Stories$custom_path --test-targets 'UITests' --test-path UITests/Stories$custom_path
+  generamba gen $module_name rvpr-controller-assembly --module-targets $project_name --module-path Assemblies/VPRs --test-targets $project_tests_target_name --test-path  $project_tests_target_name/Assemblies/VPRs
+  exit 0
+fi
+
+if [[ $command == "rm-vpr" ]]; then
+  rm -rf Assemblies/VPRs/$module_name
+  rm -rf UI/Stories$custom_path/$module_name
+  rm -rf UITests/Stories$custom_path/$module_name
+  rm -rf $project_tests_target_name/Assemblies/VPRs/$module_name
   exit 0
 fi
 
