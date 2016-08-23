65c34c22b890ea544b08e2891bbe7d01f13948af
Ian Kazlauskas <nayzak90@gmail.com>
2016-08-23 10:24:00 +0900

    added Entities target

---
+++ Entities/Entities.h
@@ -0,0 +1,19 @@
+//
+//  Entities.h
+//  Entities
+//
+//  Created by Ian Kazlauskas on 23.08.16.
+//  Copyright © 2016 nayzak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Entities.
+FOUNDATION_EXPORT double EntitiesVersionNumber;
+
+//! Project version string for Entities.
+FOUNDATION_EXPORT const unsigned char EntitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Entities/PublicHeader.h>
+
+
